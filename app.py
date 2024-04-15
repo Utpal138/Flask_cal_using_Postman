cbc9c331a4e2450fda05ee9a2e35a5e86a16710f
“Utpal <utpalanand138@gmail.com>
2024-04-15 17:56:06 +0530

    code added

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, jsonify
 
-
-app=Flask(__name__)
-
+app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return "Welcome to Flask"
 
-@app.route('/cal',methods=['GET'])
+@app.route('/cal', methods=['GET','POST'])
 def math_operation():
-    operation=request.json["operation"]
-    number1=request.json["number2"]
-    number2=request.json["number2"]
-    
-    if operation=="add":
-        result=number1+number2
-    elif operation=="multiply":
-        result=number1*number2
-    elif operation=="division":
-        result=number1/number2
-    return result
+    data = request.get_json()
+    operation = data.get("operation")
+    number1 = float(data.get("number1"))
+    number2 = float(data.get("number2"))
 
-if __name__=='__main__':
+    if operation == "add":
+        result = number1 + number2
+    elif operation == "subtract":
+        result = number1 - number2
+    elif operation == "multiply":
+        result = number1 * number2
+    else:
+        result = number1/number2
+        
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
     app.run(debug=True)
